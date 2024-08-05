dfb0a379468a55cfb5e9c2ad32e52dc89c3abfaa
Paras Garg <115867501+ParasGarg2k@users.noreply.github.com>
2024-08-05 23:47:16 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css'
 
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <ToastContainer position="bottom-center" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+  </React.StrictMode>,
+)
